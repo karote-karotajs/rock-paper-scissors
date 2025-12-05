0e0e80364b1c87f4c889aeface5dd2fea83623b3
karote-karotajs <tomijs.endzelis@gmail.com>
2025-12-05 21:55:01 +0200

    Finish project :)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 function getComputerChoice() {
     let randomNumber = Math.random() // Izveido random skaitli no 0 līdz 1. Ir 3 varianti, ko dators var izvēlēties, tātad skaitlis 1, kas ir maksimālā vērtība,
+    let computerChoice
     if (randomNumber <= 1 / 3) { // jādala ar 3. 
         computerChoice = "rock"
     }
@@ -16,18 +17,21 @@ function getComputerChoice() {
 
 function getHumanChoice() { // Izveidot loģiku, pēc kuras cilvēks var izvēlēties akmeni, šķēres vai papīru
     let choice = prompt("Rock, paper or scissors?")
+    choice.toLowerCase()
     return choice
 }
 
 let humanScore = 0; // deklarējam divus mainīgos, kuri saturēs punktu skaitu.
 let computerScore = 0;
-
+let playedGames = 0; // ar šo mainīgo skaitīs, cik spēles ir izspēlētas. Katrs playRound() inkrementē to.
 
 function playRound() {
 
     computerChoice = getComputerChoice()
 
     humanChoice = getHumanChoice()
+    
+    console.log("Dators izvēlās " + computerChoice)
 
     if (humanChoice === computerChoice) {
         console.log("Neizšķirts")
@@ -37,17 +41,34 @@ function playRound() {
         computerScore = ++computerScore
     }
     else {
-        console.log("Tu uzvarēji!")f
+        console.log("Tu uzvarēji!")
         humanScore = ++humanScore
     }
+    playedGames = ++playedGames
     console.log(`Šobrīd rezultāts ir ` + humanScore + ` pret ` + computerScore)
 }
 
+function playGame() {
+
+    while (playedGames < 5) {
+        playRound()
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Tu uzvarēji šo spēli!")
+    } else if (computerScore > humanScore) {
+        console.log("Tu zaudēji šo spēli!")
+    } else {
+        console.log("Neizšķirts!")
+    }
+}
+
+playGame()
 
 // console.log(computerChoice)
 // console.log(humanChoice)
 
-
+// Vēl noderīgi būtu loop, kamēr tiek ievadīts valid input, bet to es šobrīd nemāku :)
 
 
 
